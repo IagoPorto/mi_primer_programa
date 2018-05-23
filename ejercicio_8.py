89015fa309fff66f313e0010340e32cd2f231974
IagoPorto <iagopm8@gmail.com>
2018-05-23 23:23:38 +0200

    Añadiendo ejercicio 8

---
+++ ejercicio_8.py
@@ -0,0 +1,18 @@
+"""
+Decir el tamaño de una lsta de números escrita por el usuario sin usar la función len().
+"""
+
+lista_usuario= [1, 2, 3, 3,4 ,56, 23]
+contador=0
+
+for item in lista_usuario:
+    contador+=1
+
+print("El largo de la lista es: {}".format(contador))
+
+
+
+
+
+
+
