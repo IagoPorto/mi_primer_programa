28188f3ff418b4bca572f066de884555c46aa16c
IagoPorto <iagopm8@gmail.com>
2018-06-03 17:24:09 +0200

    Añadiendo ejercicio 23 sin terminar. Fallos en el programa

---
+++ ejercicio_23.py
@@ -0,0 +1,34 @@
+"""
+Crea un programa que al decirle el nombre de un color nos devuelva su traducción en inglés
+"""
+
+colors = {"rojo": "red", "azul": "blue", "green": "verde", "rosa": "pink"}
+choose = None
+choose_2 = None
+
+while choose != 3:
+
+    while choose != 1 and choose != 2 and choose != 3:
+        choose = int(input("¿Qué quieres hacer?: \n\t1) Obtener traducción \n\t2) Añadir color \n\t3) Salir "))
+
+    if choose == 1:
+        name_color = input("Dime el color: ")
+        keys = colors.keys()
+        for item in keys:
+            if name_color == item:
+                print(colors[name_color])
+                choose_2 = 1
+        if choose_2 != 1:
+            choose = None
+            while choose != 2 and choose != 3:
+                choose = int(input("No se ha encontrado el color, ¿quieres añadirlo? (pulsa 2 para añadir o 3 para salir): "))
+    elif choose == 2:
+        name_color = input("Dime el nombre del color en español: ")
+        translation = input("Dime la traducción del color : ")
+        colors[name_color] = name_color
+        print("El color ha sido añadido")
+    else:
+        pass
+
+
+
