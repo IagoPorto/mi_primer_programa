cb1b15ff6969217430eab929a1656bbc9302060c
IagoPorto <iagopm8@gmail.com>
2018-06-03 17:55:49 +0200

    actualizando ejercicio 23. aun tiene algun fallo

--- ejercicio_23.py
+++ ejercicio_23.py
@@ -4,7 +4,7 @@ Crea un programa que al decirle el nombre de un color nos devuelva su traducció
 
 colors = {"rojo": "red", "azul": "blue", "green": "verde", "rosa": "pink"}
 choose = None
-choose_2 = None
+choose_2 = 0
 
 while choose != 3:
 
@@ -18,15 +18,26 @@ while choose != 3:
             if name_color == item:
                 print(colors[name_color])
                 choose_2 = 1
+                choose = 0
         if choose_2 != 1:
             choose = None
             while choose != 2 and choose != 3:
                 choose = int(input("No se ha encontrado el color, ¿quieres añadirlo? (pulsa 2 para añadir o 3 para salir): "))
+        choose_2 = 0
     elif choose == 2:
         name_color = input("Dime el nombre del color en español: ")
-        translation = input("Dime la traducción del color : ")
-        colors[name_color] = name_color
-        print("El color ha sido añadido")
+        keys = colors.keys()
+        for item in keys:
+            if name_color == item:
+                print("El color ya está en la base de datos")
+                choose_2 = 1
+                choose = 0
+        if choose_2 != 1:
+            translation = input("Dime la traducción del color : ")
+            colors[name_color] = translation
+            print("El color ha sido añadido")
+            choose = 0
+            choose_2 = 0
     else:
         pass
 
